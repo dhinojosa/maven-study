5d15acbfce76c21801cc526ea4421a24bd23b476
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-06-14 23:02:12 -0600

    added wildfly

---
+++ wildfly_8.1/war-project-generic-jpa/src/main/java/com/evolutionnext/model/Album.java
@@ -0,0 +1,43 @@
+package com.evolutionnext.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Daniel Hinojosa
+ * @since 6/13/14 11:19 AM
+ * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+ * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+ * tel: 505.363.5832
+ */
+
+@Entity
+public class Album {
+
+    public Album() {
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
